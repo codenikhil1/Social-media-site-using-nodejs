e027468c8b9dae69334fabbdee949baef622d62a
unknown <niks9.ns@gmail.com>
2020-03-30 18:16:44 +0530

    comment and post done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,31 @@
-const post = require('../models/post');
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    // post.find({},function(err,posts){
+
+    // Post.find({}, function(err, posts){
     //     return res.render('home', {
-    //         title: "Home",
-    //         posts:posts
+    //         title: "Codeial | Home",
+    //         posts:  posts
     //     });
-    // })
+    // });
 
-
-    //populate the user of each post
-    post.find({}).populate('user').exec(function(err,posts){
-             return res.render('home', {
-                 title: "Home",
-                 posts:posts
-             })
-        })
+    // populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
 
 }
 
