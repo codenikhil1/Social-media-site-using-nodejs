2d0cd2c9905bb86a930d1d84a2ebd162603446b3
unknown <niks9.ns@gmail.com>
2020-03-30 15:35:43 +0530

    post complete

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
+const post = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    // post.find({},function(err,posts){
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts:posts
+    //     });
+    // })
+
+
+    //populate the user of each post
+    post.find({}).populate('user').exec(function(err,posts){
+             return res.render('home', {
+                 title: "Home",
+                 posts:posts
+             })
+        })
+
 }
 
 // module.exports.actionName = function(req, res){}
--- models/post.js
+++ models/post.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 
+
 const postSchema = new mongoose.Schema({
-    content:{
-        type:String,
-        required:true
+    content: {
+        type: String,
+        required: true
     },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+    user: {
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
     }
-}<{
-    timestamps:true
+},{
+    timestamps: true
 });
 
-const Post = mongoose.model('post',postSchema);
+const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
+
+
+
+
